eb2df4db53518140ffa1aa12331519558d9ecafa
Deepak sadhwani <108820079+Deepaksadhwani@users.noreply.github.com>
2024-06-24 16:50:33 +0530

    server routing && sending html file

--- Kickstarting Node Js/02.app.js
+++ Kickstarting Node Js/02.app.js
@@ -3,7 +3,20 @@ const http = require("http");
 
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+  const url = req.url;
+
+  console.log(req.url, req.method, req.headers);
+  res.setHeader("Content-Type", "text/html");
+  if (url == "/home") {
+    res.write("<html><body> <h1>Welcome to home page!</h1></body></html>");
+  } else if (url == "/about") {
+    res.write("<htm><body><h1>Welcome to About us page!</h1></body></htm>");
+  } else if (url == "/node") {
+    res.write("<html><body><h1>Welcome to Node js Project</h1></body></html>");
+  } else {
+    res.write("<html><body><h1>Hello world</h1></body></html>");
+  }
+  res.end();
 });
 
 server.listen(4000);
